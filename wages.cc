7b12bee56c316f4979854eff3ead3dbb18872359
Nasseef Abukamail <abukamai@ohio.edu>
2019-02-06 11:28:48 -0500

    use loops in wages

--- wages.cc
+++ wages.cc
@@ -5,38 +5,43 @@
  * Description: Add Description
  */
 
-#include <iostream>
-#include <iomanip>
 #include <cstdlib>
+#include <iomanip>
+#include <iostream>
 
 using namespace std;
 
 const int MAX_HOURS = 40;
 
 int main(int argc, char const *argv[]) {
-
-    //1
+    // 1
     double rate;
     double wages;
 
     cout << "Enter the rate: ";
     cin >> rate;
-    //2
-    double  hours;
+    // 2
+    double hours;
     cout << "Enter the number of hours: ";
     cin >> hours;
+    while (hours >= 0 && rate >= 0) {
+        // 3 calculation
+        if (hours > 40) {  // overtime
+            // true block
+            wages = (hours - MAX_HOURS) * 1.5 * rate + MAX_HOURS * rate;
+        } else {  // regular pay
+            // false block
+            wages = rate * hours;
+        }
 
-    //3 calculation
-    if (hours > 40) {   //overtime
-        //true block
-        wages = (hours - MAX_HOURS) * 1.5 * rate + MAX_HOURS * rate;
+        // 4 output
+        cout << "Wages = " << wages << endl;
+        cout << "Enter the rate: ";
+        cin >> rate;
+        // 2
+       
+        cout << "Enter the number of hours: ";
+        cin >> hours;
     }
-    else {  //regular pay
-        //false block
-        wages = rate * hours;
-    }
-
-    //4 output
-    cout << "Wages = " << wages << endl;
     return 0;
-}// main
+}  // main
