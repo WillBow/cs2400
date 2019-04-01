d61d7a3bf67a2534a9399fa9c1e3b7492df7dfb4
Nasseef Abukamail <abukamai@ohio.edu>
2019-04-01 13:15:37 -0400

    Added another print function for 2D arrays

--- 2dArrays.cc
+++ 2dArrays.cc
@@ -13,12 +13,12 @@ using namespace std;
 //function prototypes
 void printArraySingle(const int numbers[], int size);
 void printArrayDouble(const int numbers2[][4], int numRows, int numCols);
-
+void printArrayDouble2(const int numbers2[][4], int numRows, int numCols);
 int main(int argc, char const *argv[]) {
 
     int numbers[3][4] = {{3, 4, 1, 5}, {6, 7, 1, 9}, {9, 8, 0, 5}};
 
-    printArrayDouble(numbers, 3, 4);
+    printArrayDouble2(numbers, 3, 4);
 
     return 0;
 }// main
@@ -36,4 +36,18 @@ void printArraySingle(const int numbers[], int size) {
         cout << numbers[i] << " ";
     }
     cout << endl;
+}
+
+
+void printArrayDouble2(const int numbers2[][4], int numRows, int numCols) {
+
+    for(size_t i = 0; i < numRows; i++)
+    {
+        for(size_t j = 0; j < numCols; j++)
+        {
+            cout << numbers2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
 }
